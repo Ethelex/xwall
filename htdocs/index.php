c9ba16326441bb6bb326f7d5fede3a8ba066cba5
Ethelex <50265431+Ethelex@users.noreply.github.com>
2021-02-07 14:08:49 +0100

    fixed path issue

--- htdocs/index.php
+++ htdocs/index.php
@@ -3,8 +3,8 @@
 <html>
 <head>
 <title>XWall</title>
-<link href="/css/theme.css" rel="stylesheet">
-<link href="/css/base.css" rel="stylesheet">
+<link href="./css/theme.css" rel="stylesheet">
+<link href="./css/base.css" rel="stylesheet">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script>
 $('document').ready(() => {
@@ -18,7 +18,7 @@ $('document').ready(() => {
 <td>
 <div id="builder">
 <b>Post builder</b><br><br>
-<form method="post" action="/util/post.php">
+<form method="post" action="./util/post.php">
 <textarea name="message" id="builder-message" placeholder="Max 200 characters" rows="10" cols="19" maxlength="200"></textarea><br><br>
 Color: <input name="color" id="builder-color" type="color"><br><br>
 <input id="postBtn" type="submit" value="Post">
